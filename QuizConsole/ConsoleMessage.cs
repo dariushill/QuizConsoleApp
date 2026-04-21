91c5e47bae40d7407aa6f79769299e01bbe4f1c0
darius <darius_hill45@yahoo.com>
2026-04-21 18:37:13 -0400

    Add basic logic to application

---
+++ QuizConsole/ConsoleMessage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizConsole
+{
+    public class ConsoleMessage
+    {
+        public static string WelcomeMessage()
+        {
+            Console.WriteLine("Hello user welcome to my quize game");
+            Console.WriteLine("What is your name: ");
+
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        public string QuestionOne()
+        {
+
+            Console.WriteLine("What is 2 + 2?");
+
+            string userAnswer = Console.ReadLine();
+
+            return userAnswer;
+
+        }
+
+        public string QuestionTwo()
+        {
+            Console.WriteLine("What is the capital of France?");
+
+            string userAnswer = Console.ReadLine();
+
+            return userAnswer;
+        }
+
+        public string QuestionThree()
+        {
+            Console.WriteLine("What is 5 * 3?");
+
+            string userAnswer = Console.ReadLine();
+
+            return userAnswer;
+        }
+
+        public void Results(int correct, int wrong)
+        {
+            Console.WriteLine($"you have {correct} questions correct.");
+            Console.WriteLine($"you have {wrong} questions wrong");
+        }
+    }
+}
--- QuizConsole/Program.cs
+++ QuizConsole/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Everybody");
+            int correct = 0;
+            int wrong = 0;
+
+            ConsoleMessage question = new ConsoleMessage();
+
+            string answerOne = question.QuestionOne();
+            string answerTwo = question.QuestionTwo();
+            string answerThree = question.QuestionThree();
+
+            if (answerOne == "4")
+            {
+                correct++;
+            }
+            else
+            {
+                wrong++;
+            }
+
+            if (answerTwo.ToLower() == "Paris")
+            {
+                correct++;
+            }
+            else
+            {
+                wrong++;
+            }
+
+            if (answerThree == "15")
+            {
+                correct ++;
+            }
+            else
+            {
+                wrong++;
+            }
+
+            question.Results(correct, wrong);
         }
     }
 }
